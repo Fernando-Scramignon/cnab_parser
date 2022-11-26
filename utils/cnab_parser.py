24b35cf1990111d3c12159a25de30d1bf45362e6
Fernando Scramignon <scramignonnarde@gmail.com>
2022-11-26 16:15:55 -0300

    feat: adds cnab string parser

--- utils/cnab_parser.py
+++ utils/cnab_parser.py
@@ -6,7 +6,37 @@ from decimal import Decimal
 
 class CnabParser:
     @classmethod
-    def normalize_cnab(cls, file_path: str) -> list[dict]:
+    def normalize_str_cnab(cls, file_str: str) -> list[dict]:
+        if not file_str:
+            raise ValueError()
+
+        output = []
+        file_line_list = file_str.split("\n")
+
+        for line in file_line_list:
+            if not line:
+                break
+
+            transaction: dict = {}
+            transaction["type"] = line[0:1]
+            transaction["date"] = date.fromisoformat(line[1:9])
+            transaction["value"] = round(Decimal(int(line[9:19]) / 100), 2)
+            transaction["cpf"] = line[19:30]
+            transaction["card"] = line[30:42]
+            transaction["hour"] = time.fromisoformat(line[42:48])
+            transaction["shop_owner"] = line[48:62].strip()
+            transaction["shop_name"] = line[62:81].strip()
+
+            serializer = TransactionSerializer(data=transaction)
+            serializer.is_valid(raise_exception=True)
+            output.append(serializer.validated_data)
+
+        return output
+
+    @classmethod
+    def normalize_cnab(cls, file_path) -> list[dict]:
+        if not file_path:
+            raise ValueError()
         output = []
         with open(file_path, "r") as file:
             line = file.readline()
